2fb0a4dd8a52c143a432ec1f56aa6fc3630cbd91
yuta <somy.htmskl@gmail.com>
2020-07-19 17:26:57 +0900

    first commit

---
+++ minshin/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'qoutly2mgorejn*)5e+&g=!!*q-yul-@kpv3f&vb5i7jmf54xz'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
