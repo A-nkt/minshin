937a0c033c27c13974c9d887140b9359a10c45be
yuta <somy.htmskl@gmail.com>
2020-08-02 21:32:47 +0900

    for deploy

--- home/migrations/0012_auto_20200728_1847.py
+++ home/migrations/0012_auto_20200728_1847.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0011_auto_20200728_1833'),
+        ('home', '0010_auto_20200728_1827'),
     ]
 
     operations = [
