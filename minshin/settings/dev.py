be716e8a5a168b5d3a351f86853849e72b8e0496
yuta <somy.htmskl@gmail.com>
2020-08-06 13:59:52 +0900

    add new file

--- minshin/settings/dev.py
+++ minshin/settings/dev.py
@@ -1,7 +1,8 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'qoutly2mgorejn*)5e+&g=!!*q-yul-@kpv3f&vb5i7jmf54xz'
