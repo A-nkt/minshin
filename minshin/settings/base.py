4e7c656d489deb70a17ed34a686fc2c0caf3d029
yuta <somy.htmskl@gmail.com>
2020-08-06 21:30:25 +0900

    gcp

--- minshin/settings/base.py
+++ minshin/settings/base.py
@@ -166,9 +166,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 
+GS_BUCKET_NAME = 'minshin'
+
+from google.oauth2 import service_account
+
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(BASE_DIR, 'My Project 72481-3295e714262b.json'),
+)
 
 # Wagtail settings
 
