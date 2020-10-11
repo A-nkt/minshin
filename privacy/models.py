05e12f4548e27a7d20efea282972cce889a806ea
yuta <somy.htmskl@gmail.com>
2020-10-12 01:33:48 +0900

    プライバシーポリシ-

---
+++ privacy/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel,StreamFieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import StreamField
+from streams import blocks
+
+# Create your models here.
+class PrivacyPage(Page):
+    template="privacy/privacy_page.html"
+
+    rule_content=StreamField(
+        [
+            ("title",blocks.RuleTitleBlock()),
+            ("date",blocks.RuleDate()),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    content_panels=Page.content_panels+[
+        StreamFieldPanel('rule_content'),
+    ]
