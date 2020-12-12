83e7d3a6fbd86bddbcdd5e261a521522cb4acf60
YuUuTta <>
2020-12-12 17:39:17 +0900

    paginator minTO

--- minto/models.py
+++ minto/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel,StreamFieldPanel
 from wagtail.core.fields import StreamField
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from streams import blocks
 
 # 大学紹介ページ
@@ -20,6 +21,24 @@ class MintoPage(Page):
     content_panels = Page.content_panels+[
         StreamFieldPanel('UNIV'),
     ]
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        all_posts = ContentsPage.objects.live().public().order_by('-first_published_at')
+
+        paginator = Paginator(all_posts,4) #１ページ10記事
+
+        page = request.GET.get("page") #取得したページ
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+
+        context["posts"] = posts
+
+        return context
+
 # 研究科ごとのチャート
 class ContentsPage(MintoPage):
     template = "minto/contents.html"#対応するhtmlファイルを指定
