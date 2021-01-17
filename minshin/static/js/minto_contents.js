d50dc227b10bc1446cd4fbf1062df150bdd53781
YuUUTta <dsduoa31@gmail.com>
2021-01-17 11:38:01 +0900

    change snslink

--- minshin/static/js/minto_contents.js
+++ minshin/static/js/minto_contents.js
@@ -7,6 +7,7 @@ function check(){
   var input_univ = document.getElementById( "id_da-xue-ming" ).value ;
   var univ_etc = document.getElementById( "id_sonota-woxuan-ze-saretachang-he-ha-kochiraworu-li-shitekudasai" ).value ;
   var input_data = document.getElementById( "id_yan-jiu-ke" ).value ;
+  var yourself = document.getElementById( "id_u3053u81eau8eabu306bu3064u3044u3066" ).value ;
   if (input_univ == "その他"){
     if (univ_etc == "" ){
       alert("その他を選択した場合は、大学名を入力してください!");
@@ -18,7 +19,13 @@ function check(){
         return false;
       }
       else{
-        return true;
+        if (yourself == "未選択"){
+          alert("ご自身についてお答えください！");
+          return false;
+        }
+          else{
+          return true;
+        }
       }
     }
   }
@@ -28,7 +35,13 @@ function check(){
       return false;
     }
     else{
-      return true;
+      if (yourself == "未選択"){
+        alert("ご自身についてお答えください！");
+        return false;
+      }
+        else{
+        return true;
+      }
     }
   }
 }
