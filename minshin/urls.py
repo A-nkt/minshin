94651f165ce700ef6fc6cddb7d469c91e2129552
YuUUTta <dsduoa31@gmail.com>
2021-01-26 10:54:12 +0900

    use one

--- minshin/urls.py
+++ minshin/urls.py
@@ -38,7 +38,6 @@ urlpatterns = urlpatterns + [
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     url(r"", include(wagtail_urls)),#これはこのページの最後になければなりません。
-
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    url(r"^pages/", include(wagtail_urls)),
